8ef7ee7273f4c3c290822908e3108ba37f574f84
Cristian <cefunck@miuandes.cl>
2019-03-28 17:03:39 -0300

    upload most_expensive_ticket_bought method

--- blog/app/models/user.rb
+++ blog/app/models/user.rb
@@ -1,13 +1,12 @@
 class User < ApplicationRecord
   has_many :orders
-  def most_expensive_ticket
-    order = Order.all
-    ticketor = TicketOrder.all
-    m1 = order.joins(ticketor)
-    tick = Ticket.all
-    m2 = m1.joins(tick)
-    user_tickets = m2.where("user_id = ?", self.id)
-    max_amount = user_tickets.maximum("price")
-    return max_amount
+  def most_expensive_ticket_bought
+    prices = []
+    self.orders.each do |order|
+        order.tickets.each do |ticket|
+          prices.push(ticket.price)
+        end
+    end
+    return prices.max
   end
 end
