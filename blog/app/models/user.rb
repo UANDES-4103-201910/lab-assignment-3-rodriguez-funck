662afee38a2bfd46f30d1475243a9fa976c2170c
Cristian <cefunck@miuandes.cl>
2019-03-28 17:24:33 -0300

    upload most_expensive_ticket_bought_between method

--- blog/app/models/user.rb
+++ blog/app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   has_many :orders
+
   def most_expensive_ticket_bought
-    prices = []
+    prices = [0]
     self.orders.each do |order|
         order.tickets.each do |ticket|
           prices.push(ticket.price)
@@ -9,4 +10,15 @@ class User < ApplicationRecord
     end
     return prices.max
   end
+
+  def most_expensive_ticket_bought_between(start_date,end_date)
+    prices = [0]
+    self.orders.each do |order|
+        order.tickets.where(:created_at => start_date.beginning_of_day..end_date.end_of_day).each do |ticket|
+          prices.push(ticket.price)
+        end
+    end
+    return prices.max
+  end
+
 end
