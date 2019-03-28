b76cd8debe9d0968993e0a7fc278340ed83679b6
Cristian <cefunck@miuandes.cl>
2019-03-28 19:44:17 -0300

    upload last_attendance method

--- blog/app/models/venue.rb
+++ blog/app/models/venue.rb
@@ -1,3 +1,14 @@
 class Venue < ApplicationRecord
   has_many :events
+
+  def last_attendance
+    attendance = 0
+    self.events.order("created_at").last.tickets.each do |tickets|
+      tickets.ticket_orders.each do |ticket_order|
+        attendance += ticket_order.amount
+      end
+    end
+    return attendance
+  end
+
 end
